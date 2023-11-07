670d909c90e60aa94f2b5e75e269719d52cd2815
Admin <paleclandusha@gmail.com>
2023-11-07 22:08:02 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-arr = []
+arr = [1, 2, 3]
 a = int(len(arr) / 2)
 b = int((1 + len(arr) / 2))
 if len(arr) % 2 == 0:
     new_arr = ([arr[:a], arr[a:]])
-    print(new_arr)
-elif len(arr) % 2 == 1:
-    new_arr = ([arr[:b], arr[b:]])
-    print(new_arr)
 else:
-    new_arr = ([], [])
-    print(new_arr)
+    new_arr = ([arr[:b], arr[b:]])
+print(new_arr)
